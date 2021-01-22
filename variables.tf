7b1df63a1b0ec82eb94640817b3f0d1e5954a1c1
Stephen Moss <bladexr8@gmail.com>
2021-01-22 22:53:44 +1100

    clean up variables file

--- variables.tf
+++ variables.tf
@@ -10,12 +10,6 @@ variable "contact" {
   default = "bladexr8@gmail.com"
 }
 
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 80
-}
-
 variable "default_ec2_instance_type" {
   description = "The default EC2 instance type tp create"
   type        = string
